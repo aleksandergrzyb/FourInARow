46caa6f29e8ead84c709d3549576e1cd8b9c6012
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2013-01-09 22:46:38 +0100

    First commit of application.

---
+++ Code/_config/config.lua
@@ -0,0 +1,1 @@
+application = {    content =     {         width = 320,        height = 480,         scale = "letterbox",        fps = 60,        imageSuffix =        {            ["@2x"] = 1.5,        },        antialias = false,    },}
